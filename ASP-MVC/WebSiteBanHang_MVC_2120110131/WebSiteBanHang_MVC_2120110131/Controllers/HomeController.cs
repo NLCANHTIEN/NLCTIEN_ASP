7165e78c27ee5cd607f58261b2ef8c8ba774b63e
NLCANHTIEN <nguyenlecanhtien120602.work@gmail.com>
2023-09-29 14:56:29 +0700

    first commit

---
+++ ASP-MVC/WebSiteBanHang_MVC_2120110131/WebSiteBanHang_MVC_2120110131/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebSiteBanHang_MVC_2120110131.Context;
+using WebSiteBanHang_MVC_2120110131.Models;
+
+namespace WebSiteBanHang_MVC_2120110131.Controllers
+{
+    public class HomeController : Controller
+    {
+        WebsiteBanHangEntities objWebsiteBanHangEntities = new WebsiteBanHangEntities();
+        public ActionResult Index()
+        {
+            HomeModel objHomeModel = new HomeModel();
+
+            objHomeModel.ListCategory = objWebsiteBanHangEntities.Category.ToList();
+
+            objHomeModel.ListProduct = objWebsiteBanHangEntities.Product.ToList();
+            return View(objHomeModel);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
